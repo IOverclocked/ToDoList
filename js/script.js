04a56ca6c180b901cef4481f593aa5960c1424d6
IOverclocked <osiatycki.m@gmail.com>
2018-03-20 10:31:11 +0100

    Added event show/hide form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(e) {
-    // body...
+
+    var btnShowForm = $('i.icon-add'),
+        form = $('header form'),
+        formContent = $('header form > div');
+
+    //event dla pokazywania i ukrywania formularza
+    btnShowForm.on('click', function(){
+
+        form.animate({
+            width: "toggle"
+        })
+        formContent.slideToggle("slow");
+
+    });
+
+
+
+
 });
